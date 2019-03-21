968846ed45770cb6f75b8cee2484549302d4d911
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-03-21 11:18:03 +0000

    Return buzz if number = 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def fizzbuzz(number)
-  "fizz"
+  if number == 3
+    "fizz"
+  elsif number == 5
+    "buzz"
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe "Fizzbuzz" do
   it "returns 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it "returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
